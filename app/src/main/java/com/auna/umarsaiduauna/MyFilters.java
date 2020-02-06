e4240a2606daccb3a2768bffb57e6e042f9d705a
Umar Saidu Auna <umarmanofpeace@gmail.com>
2020-02-06 11:43:09 +0100

    feat: 🎸 Dashboard completed

--- app/src/main/java/com/auna/umarsaiduauna/MyFilters.java
+++ app/src/main/java/com/auna/umarsaiduauna/MyFilters.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MyFIlters extends AppCompatActivity {
+public class MyFilters extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_filters);
+        setTitle("My Filters");
     }
 }
