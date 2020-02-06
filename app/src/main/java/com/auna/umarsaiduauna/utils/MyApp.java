43145b97fad7f44c4acf67e00af8822f9b070fc8
Umar Saidu Auna <umarmanofpeace@gmail.com>
2020-02-06 11:03:52 +0100

    feat: 🎸 Working on Dashboard

---
+++ app/src/main/java/com/auna/umarsaiduauna/utils/MyApp.java
@@ -0,0 +1,22 @@
+package com.auna.umarsaiduauna.utils;
+
+import android.app.Application;
+import android.content.Context;
+
+public class MyApp  extends Application {
+    private static MyApp instance;
+
+    public static MyApp getInstance(){
+        return instance;
+    }
+
+    public static Context getContext(){
+        return instance.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        instance = this;
+        super.onCreate();
+    }
+}
